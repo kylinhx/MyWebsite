d74811fb17c8df686e8973c2b951665eaee86692
kylinhanx <kylinhanx@gmail.com>
2023-11-29 20:04:30 +0800

    11.29

--- app.py
+++ app.py
@@ -2,12 +2,11 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='static/html', static_folder='static')
 
-@app.route('/')
+@app.route('/resume')
 def homepage():
     # 我的html路径为static\html\index.html
 
     return render_template('index.html')
 
 if __name__ == '__main__':
-    # BEGIN: ed8c6549bwf9
     app.run(port=80)
